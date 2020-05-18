3106d97de7f9a945b753138ca1c452dce114b843
mmaistro <mm@di.ku.dk>
2020-05-18 10:26:54 +0200

    Update TransformLink.js

--- javascript/TransformLink.js
+++ javascript/TransformLink.js
@@ -1,5 +1,5 @@
 // Function to change the link and title content once they are clicked
-document.getElementById(pastedition_link).onclick = function(){
+document.getElementById("pastedition_link").onclick = function(){
 	// Get the title and anchor elements
 	var title = document.getElementById("pastedition_title");
 	var link = document.getElementById("pastedition_link");
@@ -11,10 +11,12 @@ document.getElementById(pastedition_link).onclick = function(){
 		title.textContent = " 2020 Track Overview ";
 		link.textContent = "For 2020 overview, click here.";
 		link.href = "index.html";
+		break;
 	} else if (title_content == " 2020 Track Overview ") {
 		// Change title and link to a the 2019 edition
 		title.textContent = " 2019 Track Overview ";
 		link.textContent = "For 2019 overview, click here.";
 		link.href = "2019.html";
+		break;
 	}
 }
