111ceeb086151626b20bd379670cdd47ba5c6560
Maria Maistro <tjz514@ku.dk>
2020-05-18 10:03:50 +0200

    Javascript Folder added

---
+++ javascript/TransformLink.js
@@ -0,0 +1,20 @@
+// Function to change the link and title content once they are clicked
+function transformLink(){
+	// Get the title and anchor elements
+	var title = document.getElementById("pastedition_title");
+	var link = document.getElementById("pastedition_link");
+	// Get the content of each element
+	var title_content = title.textContent;
+	// Check the content for the title tag
+	if (title_content == " 2019 Track Overview ") {
+		// Change title and link to a the 2020 edition
+		title.textContent = " 2020 Track Overview ";
+		link.textContent = "For 2020 overview, click here.";
+		link.href = "index.html";
+	} else if (title_content == " 2020 Track Overview ") {
+		// Change title and link to a the 2019 edition
+		title.textContent = " 2019 Track Overview ";
+		link.textContent = "For 2019 overview, click here.";
+		link.href = "2019.html";
+	}
+}
